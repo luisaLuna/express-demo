987db9f72efe9a74cfe78e942b7b9427ead554fc
luisaLuna <luisacasaslu@gmail.com>
2021-04-03 10:19:33 -0500

    Routing Basics

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express()
 
-app.use((req, res) => {
-    console.log("NEW REQUEST!");
-    res.send("<h1>This is your response!</h1>");
-})
+
+// app.use((req, res) => {
+//     console.log("NEW REQUEST!");
+//     res.send("<h1>This is your response!</h1>");
+// })
+
+ 
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to our home page!</h1>');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact us!</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1>');
+});
+
+app.post('/about', (req, res) => {
+    res.send('POST REQUEST ON /about (This is different than a get request!');
+});
+
+
+app.get('*', (req, res) => {
+    res.send("I don't know that route");
+});
+
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!");
