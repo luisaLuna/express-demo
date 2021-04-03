14111b146e261f861980ed30fe1577ad8100aa1a
luisaLuna <luisacasaslu@gmail.com>
2021-04-03 08:50:29 -0500

    Request & Response

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express()
 
+app.use((req, res) => {
+    console.log("NEW REQUEST!");
+    res.send("<h1>This is your response!</h1>");
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!");
 })
+
